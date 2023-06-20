2d185f146182a566a3543c893036187c22e4883c
edmharalcones <134608637+edmharalcones@users.noreply.github.com>
2023-06-21 01:59:48 +0800

    modified:   index.html 	new file:   resources/background2.png 	new file:   resources/facebook.png 	new file:   resources/instagram.png 	new file:   resources/messenger.png 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -36,30 +36,42 @@ function makeDraggable(element) {
   
 
   makeDraggable(document.getElementById('notepad'));
+  makeDraggable(document.getElementById('todo'));
   
 
 
 
   function notepadbtn() {
-    var x = document.getElementById("notepad");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    var NotepadView = document.getElementById("notepad");
+    if (NotepadView.style.display === "block") {
+      NotepadView.style.display = "none";
     } else {
-      x.style.display = "none";
+      NotepadView.style.display = "block";
     }
   }
 
-  // Timer
+  function todobtn() {
+    var TodoView = document.getElementById("todo");
+    if (TodoView.style.display === "block") {
+      TodoView.style.display = "none";
+    } else {
+      TodoView.style.display = "block";
+    }
+  }
+
+
   
     let countdown;
     let targetTime;
     let paused = false;
-    let initialTime = 25 * 60 * 1000; // 25 minutes in milliseconds
+    let initialTime = 25 * 60 * 1000; 
+    let reset = 1;
 
     function Pomodoro() {
       clearInterval(countdown);
       countdown = null;
       paused = false;
+      reset = 1;
       document.getElementById("timer").innerHTML = "25:00";
       initialTime = 25 * 60 * 1000;
     }
@@ -68,6 +80,7 @@ function makeDraggable(element) {
       clearInterval(countdown);
       countdown = null;
       paused = false;
+      reset = 2;
       document.getElementById("timer").innerHTML = "05:00";
       initialTime = 5 * 60 * 1000;
     }
@@ -76,8 +89,9 @@ function makeDraggable(element) {
       clearInterval(countdown);
       countdown = null;
       paused = false;
-      document.getElementById("timer").innerHTML = "15:00";
-      initialTime = 15 * 60 * 1000;
+      reset = 3;
+      document.getElementById("timer").innerHTML = "60:00";
+      initialTime = 60 * 60 * 1000;
     }
 
     function startTimer() {
@@ -88,19 +102,25 @@ function makeDraggable(element) {
       }
     }
 
-    function stopTimer() {
-      if (countdown) {
-        clearInterval(countdown);
-        countdown = null;
-        paused = true;
-      }
-    }
 
     function resetTimer() {
       clearInterval(countdown);
       countdown = null;
       paused = false;
+
+      if (reset == 1){
       document.getElementById("timer").innerHTML = "25:00";
+      }
+
+      else if (reset == 2) {
+        document.getElementById("timer").innerHTML = "5:00";
+
+      }
+
+      else{
+        document.getElementById("timer").innerHTML = "60:00"; 
+      }
+
     }
 
     
@@ -110,14 +130,11 @@ function makeDraggable(element) {
         const now = new Date().getTime();
         const timeRemaining = targetTime - now;
 
-        // Calculate minutes and seconds
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-        // Display the time in the "timer" element
         document.getElementById("timer").innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
-        // If the countdown is finished, clear the interval
         if (timeRemaining <= 0) {
           clearInterval(countdown);
           countdown = null;
@@ -126,3 +143,59 @@ function makeDraggable(element) {
       }
     }
   
+    // Todo
+
+    var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
